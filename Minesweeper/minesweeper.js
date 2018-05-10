f2a4152a1baa16ea2fbb5b5da3f590b0270073be
Alex Rogers <alex.rogers823@gmail.com>
2018-05-10 09:19:26 -0400

    Recreating Minesweeper using JS

---
+++ Minesweeper/minesweeper.js
@@ -0,0 +1,13 @@
+function createBoard(mode = 10) {
+    for (let i = 0; i < mode; i++) {
+      // create row
+      for (let j = 0; j < mode; j++) {
+        // create column
+      }
+    }
+    const randomizeMines = mode => {
+      // function to put in mines at random
+      // certain number of mines based on mode
+    }
+    // Then find a way to set in numbers based on adjacent mines
+}
