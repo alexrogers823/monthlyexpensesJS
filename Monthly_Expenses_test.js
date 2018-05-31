f63490462c934cd591be37f0098b20a602433438
Alex Rogers <alex.rogers823@gmail.com>
2018-05-30 22:57:10 -0400

    Added basic describe/it functions

--- Monthly_Expenses_test.js
+++ Monthly_Expenses_test.js
@@ -1,1 +1,15 @@
 // This is where unit testing will happen with monthly expenses (with describe/it functions)
+
+describe('Summary Calculation', ()=> {
+  it('should see if individual cell is calculated', ()=>{
+    //put something here
+  });
+
+  it('should check if totals are calculating', ()=>{
+    //something here
+  });
+
+  it('should translate current month numbers into graph', ()=> {
+    //something here
+  });
+});
