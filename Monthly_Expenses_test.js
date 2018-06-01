bdb9eb0c854bb646daddeff765917cba490b9ad9
Alex Rogers <alex.rogers823@gmail.com>
2018-05-31 22:54:54 -0400

    Modified tests to show pending status

--- Monthly_Expenses_test.js
+++ Monthly_Expenses_test.js
@@ -1,15 +1,9 @@
 // This is where unit testing will happen with monthly expenses (with describe/it functions)
 
 describe('Summary Calculation', ()=> {
-  it('should see if individual cell is calculated', ()=>{
-    //put something here
-  });
+  it('should see if individual cell is calculated');
 
-  it('should check if totals are calculating', ()=>{
-    //something here
-  });
+  it('should check if totals are calculating');
 
-  it('should translate current month numbers into graph', ()=> {
-    //something here
-  });
+  it('should translate current month numbers into graph');
 });
