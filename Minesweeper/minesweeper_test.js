d05d809a64c7728a1a404784bea764d92f955282
Alex Rogers <alex.rogers823@gmail.com>
2018-06-01 14:36:49 -0400

    testing for functions for minesweeper

---
+++ Minesweeper/minesweeper_test.js
@@ -0,0 +1,9 @@
+describe('setting up framework', ()=>{
+  it('should connect to MongoDB');
+
+  it('should create outline for cards');
+
+  it('should display current date');
+
+  it('should display header and footer');
+});
