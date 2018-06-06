47b98f01289d42a20c9b8ed7e17f1e801fa01445
Alex Rogers <alex.rogers823@gmail.com>
2018-06-05 21:33:32 -0400

    More tests based on month pages

--- Monthly_Expenses_test.js
+++ Monthly_Expenses_test.js
@@ -7,3 +7,11 @@ describe('Summary Calculation', ()=> {
 
   it('should translate current month numbers into graph');
 });
+
+describe('Month Accumulation', () => {
+  it('should recognize individual price as a float');
+
+  it('should add current ind price with one before it');
+
+  it('should calculate in real time');
+});
