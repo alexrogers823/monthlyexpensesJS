fc5df4a52dec2c6f4428f377f8bd9906a6a44b95
Alex Rogers <alex.rogers823@gmail.com>
2018-07-02 16:00:30 -0400

    Converted to internal JS; working on importing

--- InteractiveBarGraph/app.js
+++ InteractiveBarGraph/app.js
@@ -1,6 +1,8 @@
 const http = require("http");
+const router = require("./router.js");
 
 http.createServer((req, res) => {
   // connect it to the main page here
+  router.home(req, res);
 }).listen(3000, '127.0.0.1');
 console.log('Server running at http://127.0.0.1:3000');
